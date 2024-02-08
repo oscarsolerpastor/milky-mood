e8e3441cebed5d5151622cd13e346acc01f3f123
Óscar Soler <42406924+oscarsolerpastor@users.noreply.github.com>
2024-02-08 14:58:17 +0100

    Initial commit

---
+++ src/components/ModalInner/index.jsx
@@ -0,0 +1,46 @@
+import React, {useState} from 'react'
+import {Container, H3, P, Socials, H4, SuccessContainer} from './styles'
+import Form from '../Form'
+import {ReactComponent as Happy} from '../../../public/happy_face.svg'
+import {ReactComponent as Instagram} from '../../../public/icon_facebook.svg'
+import {ReactComponent as Facebook} from '../../../public/icon_instagram.svg'
+import {ReactComponent as Twitter} from '../../../public/icon_twitter.svg'
+
+const Input = ({handleSuccess}) => (
+  <Container>
+    <H3>Sign Up</H3>
+    <P>
+      Join our newsletter to be notified of when pre-release tickets for our
+      first ever world tour go on sale.
+    </P>
+    <Form handleSuccess={handleSuccess} />
+  </Container>
+)
+
+const Success = () => (
+  <SuccessContainer>
+    <Happy />
+    <H4>Yay! We're so excited!</H4>
+    <P>
+      We hope you are too. In the meantime you can follow us on social media to
+      keep up to do date:
+    </P>
+    <Socials>
+      <Instagram />
+      <Twitter />
+      <Facebook />
+    </Socials>
+  </SuccessContainer>
+)
+
+const ModalInner = () => {
+  const [showSuccess, setShowSuccess] = useState(false)
+
+  const handleSuccess = () => {
+    setShowSuccess(true)
+  }
+
+  return showSuccess ? <Success /> : <Input handleSuccess={handleSuccess} />
+}
+
+export default ModalInner
